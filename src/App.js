6c98058c58a14965ac987e441b359542c2f66b7e
oscarjes <oscar@jesionek.co>
2017-11-17 13:42:23 +0700

    finish video

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import TweetBox from './TweetBox';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      tweets: ['Hello World!', 'CoderSchool rocks.']
+    }
+  }
+
+  handleTweet(tweet){
+    this.setState({
+      tweets: this.state.tweets.concat(tweet)
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to CoderSchool Prework</h1>
+        </header>
+        <div>
+          <TweetBox prompt="What's your status?" onTweet={this.handleTweet.bind(this)}/>
+        </div>
+        <div>
+          { this.state.tweets.map( tweet => 
+            <p> {tweet} </p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/TweetBox.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class TweetBox extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    text: "",
+    charsRemaining: 140
+  }
+}
+
+  handleChange(text){
+    this.setState(
+      { text: text ,
+      charsRemaining: 140 - text.length
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" 
+          placeholder={this.props.prompt}
+          onChange={(e) => this.handleChange(e.target.value)}
+        />
+        <p>
+          { this.state.charsRemaining}
+        </p>
+        <button 
+          onClick={() => this.props.onTweet(this.state.text)}
+          disabled={ this.state.charsRemaining < 0 }
+        >
+          Tweet{" "}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TweetBox;
